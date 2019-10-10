9cfff2d2ce17f3bcebb3d80cf3711f5135bfe1fb
benjivou <benjivou2@gmail.com>
2019-10-10 11:23:28 +0200

    Add Gson in the project

--- src/handler/TimeHandler.java
+++ src/handler/TimeHandler.java
@@ -3,9 +3,16 @@ package handler;
 import abstracts.HandlerAbstract;
 
 public class TimeHandler extends HandlerAbstract {
+    private int idTarget;
 
-
-    public TimeHandler() {
+    public TimeHandler(long waitTime, int idTarget) {
         super();
+        this.waitingTime = waitTime;
+        this.idTarget = idTarget;
+    }
+
+    @Override
+    protected void onAction() {
+        super.onAction();
     }
 }
